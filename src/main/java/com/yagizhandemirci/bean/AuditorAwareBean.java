d43715b3e52b74a66d6e7706b02e0a1b11ce06d0
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-05-16 02:20:18 +0300

    Auditor ile ilgili işlemler yapıldı. İşlemleri yapan kullanıcıyı databaseden çekemiyor.

---
+++ src/main/java/com/yagizhandemirci/bean/AuditorAwareBean.java
@@ -0,0 +1,14 @@
+package com.yagizhandemirci.bean;
+
+import com.yagizhandemirci.audit.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+@Configuration
+public class AuditorAwareBean {
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return new AuditorAwareImpl();
+    }
+}
